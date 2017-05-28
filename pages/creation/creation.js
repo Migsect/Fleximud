26fce343a6f739bc387cde0b4db4c7069a9a623e
Alex <alsamong@gmail.com>
2017-05-28 16:25:08 -0400

    Added connect-session-knex to package Setup templates for creation page

--- pages/creation/creation.js
+++ pages/creation/creation.js
@@ -1,6 +1,9 @@
 "use strict";
 
+const templates = require(process.cwd() + "/templates/templates");
+const pageTemplate = templates(process.cwd() + "/pages/creation/creation");
+
 module.exports = function renderer()
 {
-
+    return pageTemplate();
 };
